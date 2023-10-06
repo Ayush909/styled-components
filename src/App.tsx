ff98f677e3f5172bceb2964e4508e64671e05bed
Ayush Singh Kushwaha <50234003+Ayush909@users.noreply.github.com>
2023-10-06 23:39:49 +0530

    typscript , mui , typography

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import { MuiTypography } from "./components/MuiTypography";
+
+function App() {
+  return (
+    <>
+      <MuiTypography />
+    </>
+  );
+}
+
+export default App;
